82ffa95a7a1a77be00113c4329cdc499994d96fe
逆風的薔薇 <riayang.cn@gmail.com>
2019-10-15 21:38:53 -0700

    Update InsertionSort.java

--- Algorithms/Sort/InsertionSort.java
+++ Algorithms/Sort/InsertionSort.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * 插入排序
  * 空间复杂度 O(1) - 原地排序
  * 相同大小数据相对位置不变 - 稳定排序
- * 最好时间复杂度O(n^2)，最坏O(n^2)，平均O(n^2)
+ * 最好时间复杂度O(n)，最坏O(n^2)，平均O(n^2)
  */
 public class InsertionSort {
      public void insertionSort(int[] array) {
@@ -19,7 +19,7 @@ public class InsertionSort {
          // 从第二个元素开始，为每个元素寻找插入位置
          for (int i=1; i<len; ++i) {
              int value = array[i];
-
+             // 从有序组从尾到头遍历
              int j = i - 1;
              for (; j>=0; --j) {
                  if (array[j] > value) {
